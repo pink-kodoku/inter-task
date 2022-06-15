af988eb77047de113f635662fd0d0f39fa445559
Peter <petr.dyuskin@mail.ru>
2022-06-15 10:18:46 +0500

    add icons for arrows

--- public/hooks/useActions.ts
+++
@@ -1,9 +0,0 @@
-import {useDispatch} from "react-redux";
-import {bindActionCreators} from "redux";
-import {actionsCreators} from "../state"
-
-export const useActions = () => {
-  const dispatch = useDispatch();
-
-  return bindActionCreators(actionsCreators, dispatch);
-}
